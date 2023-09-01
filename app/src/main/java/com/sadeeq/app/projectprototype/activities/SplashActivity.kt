37d1b8a2765d9145d329525a7c9bd00b147b2c52
sadeeq <sadeeqrahman1@gmail.com>
2023-09-01 15:23:12 +0400

    image sharing done

--- app/src/main/java/com/sadeeq/app/projectprototype/activities/SplashActivity.kt
+++ app/src/main/java/com/sadeeq/app/projectprototype/activities/SplashActivity.kt
@@ -17,14 +17,51 @@ class SplashActivity : AppCompatActivity() {
 
         //startActivity(Intent(this,ImageSliderActivity::class.java))
 
-        val image: ImageView = findViewById(R.id.imageview)
-        if (Intent.ACTION_SEND == intent.action && intent.type?.startsWith("image/") == true) {
-            val imageUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
-            if (imageUri != null) {
-                Glide.with(this)
-                    .load(imageUri)
-                    .into(image)
+        if (Intent.ACTION_SEND == intent.action) {
+            val type = intent.type
+
+            if (type != null) {
+                when {
+                    type.startsWith("image/") -> {
+                        val imageUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                        if (imageUri != null) {
+                            // Handle the shared image here
+                            displayImage(imageUri)
+                        }
+                    }
+                    type.startsWith("video/") -> {
+                        val videoUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                        if (videoUri != null) {
+                            // Handle the shared video here
+                            playVideo(videoUri)
+                        }
+                    }
+                    type.startsWith("application/pdf") -> {
+                        val pdfUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                        if (pdfUri != null) {
+                            // Handle the shared PDF document here
+                            openPdfDocument(pdfUri)
+                        }
+                    }
+                    // Handle other document types as needed
+                    else -> {
+                        // Handle other types of shared documents here
+                    }
+                }
             }
         }
+    }
+
+    private fun displayImage(imageUri: Uri) {
+        // Your code to display the shared image
+    }
+
+    private fun playVideo(videoUri: Uri) {
+        // Your code to play the shared video
+    }
+
+    private fun openPdfDocument(pdfUri: Uri) {
+        // Your code to open and display the shared PDF document
+
     }
 }
