9503c65f8262cbc4e6038c5389e91c6fd2a6c65f
sadeeq <sadeeqrahman1@gmail.com>
2023-09-01 12:15:42 +0400

    image sharing done

--- app/src/main/java/com/sadeeq/app/projectprototype/activities/SharedImageActivity.kt
+++ app/src/main/java/com/sadeeq/app/projectprototype/activities/SharedImageActivity.kt
@@ -5,25 +5,18 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.sadeeq.app.projectprototype.R
-import com.sadeeq.app.projectprototype.activities.alaram.AlarmActivity
-import com.sadeeq.app.projectprototype.activities.nfc.NFCActivity
 
-class SplashActivity : AppCompatActivity() {
+class SharedImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_shared_image)
 
-        //startActivity(Intent(this,ImageSliderActivity::class.java))
-
-        val image: ImageView = findViewById(R.id.imageview)
+        val image:ImageView = findViewById(R.id.imageview)
         if (Intent.ACTION_SEND == intent.action && intent.type?.startsWith("image/") == true) {
             val imageUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
             if (imageUri != null) {
-                Glide.with(this)
-                    .load(imageUri)
-                    .into(image)
+                image.setImageURI(imageUri)
             }
         }
     }
